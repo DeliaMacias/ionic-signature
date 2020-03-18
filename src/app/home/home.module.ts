43462834bd8a82b0660288c0cf00c76597c14621
TheMickyeBMO <35576875+DeliaMacias@users.noreply.github.com>
2020-03-17 21:53:09 -0700

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+// aqui
+import { SignaturePadModule } from 'angular2-signaturepad';
 
 import { HomePage } from './home.page';
 
 @NgModule({
+  // aqui
+  providers: [ SignaturePadModule],
+
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    // aqui
+    SignaturePadModule,
+
     RouterModule.forChild([
       {
         path: '',
