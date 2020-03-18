08c62ea3394fa89c6ba2a3582fda3d0d50d969c9
TheMickyeBMO <35576875+DeliaMacias@users.noreply.github.com>
2020-03-17 21:54:17 -0700

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild, Renderer } from '@angular/core';
+
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  url: string;
+  @ViewChild(SignaturePad, {static: false }) signaturePad: SignaturePad;
 
-  constructor() {}
+  // tslint:disable-next-line: ban-types
+  private signaturePadOptions: Object = { // passed through to szimek/signature_pad constructor
+    maxWidth: 1,
+    minWidth: 1,
+    canvasWidth: 300,
+    canvasHeight: 200
+  };
+
+  constructor( ) {}
+
+  drawStart() {
+  }
+  drawComplete() {
+    console.log(this.signaturePad.toDataURL());
+  }
+  clear() {
+    this.signaturePad.clear();
+  }
+  save() {
+    this.url = this.signaturePad.toDataURL();
+  }
 
 }
